14e2039289d1e2c2ec8336701cb0464ed164e651
Renan Perin Ribeiro <renan.perinribeiro@outlook.com>
2023-01-10 15:15:29 -0300

    services

--- curso-angular/src/app/components/list-render/list-render.component.ts
+++ curso-angular/src/app/components/list-render/list-render.component.ts
@@ -2,12 +2,19 @@ import { Component } from '@angular/core';
 
 import { Animal } from 'src/app/Animal';
 
+import { ListService } from 'src/app/services/list.service';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css']
 })
 export class ListRenderComponent {
+
+  constructor(private listService: ListService) {
+
+  }
+
   animals: Animal[] = [
     {name: "Turca", type: "dog", age: 12},
     {name: "Mel", type: "cat", age: 3},
@@ -19,5 +26,10 @@ export class ListRenderComponent {
 
   showAge(animal: Animal): void {
     this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos.`
+  }
+
+  removeAnimal(animal: Animal) {
+    console.log("removendo animal")
+    this.animals = this.listService.remove(this.animals, animal)
   }
 }
